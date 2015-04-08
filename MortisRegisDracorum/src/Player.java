b9182f4da79dfca3be0bf76aa4cee2e57a720ad4
Alexander Rehnman <globox85@gmail.com>
2015-04-08 12:43:59 +0200

    De senaste dagarnas arbete.

---
+++ MortisRegisDracorum/src/Player.java
@@ -0,0 +1,24 @@
+
+public class Player {
+	
+	int level = 1;
+	int health, mana, fury, energy, toughness, dexterity, intelligence, strength;
+	String player_class;
+	
+	
+	public void setClass(String string) {
+		player_class = string;
+		
+		if(string == "Warrior"){
+			strength = 4;
+			toughness = 3;
+			dexterity = 2;
+			intelligence = 1;
+		}
+		
+		
+		
+	}
+	
+
+}
